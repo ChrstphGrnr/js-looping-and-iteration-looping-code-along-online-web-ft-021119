9d00a5b9574541908e75041c138e3421a724b3e9
Christoph Gruener <christophgruener@me.com>
2019-05-25 16:31:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 
 function writeCards(names, event) {
   for (let i = 0; i < names.length; i++) {
-    console.log(`Thank you ${name[1]} for the amazinf ${event} gift!`)
+    `Thank you ${name[1]} for the amazinf ${event} gift!`
   }
 }
